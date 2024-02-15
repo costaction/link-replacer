3f82a25524927acd566c033b24c9af0c48fdc884
Costaction <noreply@costaction.com>
2024-02-15 19:43:50 +0300

    Automatic update

--- simple.js
+++ simple.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const sourceDomains = [ 'mirror2fd1.local.dev.golastik.com', 'fd1.local.dev.golastik.com' ];
-    const targetDomain = 'short1fd1.local.dev.golastik.com';
+    const sourceDomains = [ 'turbobit.cc', 'turbobit.website', 'turbo-bit.website', 'turbobit1.com', 'turbobits.net', 'turbobyte.net', 'turbobit2.pw', 'turbo2bit.com', 'turbobit5.net', 'turbobi.pw', 'turbobitt.net', '2turbobit.net', 'trubobit.com', 'turboobit.com', 'turbobbt.net', 'turbosit.net', 'turbobiet.com', 'turboblt.com', 'turbobiyt.net', 'turbobif.com', 'turbobite.net', 'tourbobit.com', 'turbobitn.com', 'turbobyt.net', 'tuerbobit.net', 'turbobeet.net', 'turbobiit.net', 'turbodit.net', 'tourbobit.net', 'turbobitt.com', 'turbobits.cc', 'turbobite.me', 'turbobit.net' ];
+    const targetDomain = 'tbit.to';
 
     for(let i = 0; i < document.links.length; i++) {
         let hostname = document.links[i].hostname.replace(/^www\./, '');
