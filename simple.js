8feded55df2dfe40df773e6cd1e47f7168416ab0
Costaction <noreply@costaction.com>
2024-02-16 13:11:42 +0300

    Automatic update

--- simple.js
+++ simple.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const sourceDomains = [ 'turbobit.cc', 'turbobit.pw', 'turbobit.website', 'turbo-bit.website', 'turbobit1.com', 'turbobits.net', 'turbobyte.net', 'turbobit2.pw', 'turbo2bit.com', 'turbobit5.net', 'turbobi.pw', 'turbobitt.net', '2turbobit.net', 'trubobit.com', 'turboobit.com', 'turbobbt.net', 'turbosit.net', 'turbobiet.com', 'turboblt.com', 'turbobiyt.net', 'turbobif.com', 'turbobite.net', 'tourbobit.com', 'turbobitn.com', 'turbobyt.net', 'tuerbobit.net', 'turbobeet.net', 'turbobiit.net', 'turbodit.net', 'tourbobit.net', 'turbobitt.com', 'turbobits.cc', 'turbobite.me', 'turbobit.net' ];
+    const sourceDomains = [ 'turbobit.cc', 'turbobit.pw', 'turbobit.website', 'turbo-bit.website', 'turbobit1.com', 'turbobits.net', 'turbobyte.net', 'turbobit2.pw', 'turbo2bit.com', 'turbobit5.net', 'turbobi.pw', 'turbobitt.net', '2turbobit.net', 'trubobit.com', 'turboobit.com', 'turbosit.net', 'turbobiet.com', 'turboblt.com', 'turbobiyt.net', 'turbobif.com', 'turbobite.net', 'tourbobit.com', 'turbobitn.com', 'turbobyt.net', 'tuerbobit.net', 'turbobeet.net', 'turbobiit.net', 'turbodit.net', 'tourbobit.net', 'turbobitt.com', 'turbobits.cc', 'turbobite.me', 'turbobit.net' ];
     const targetDomain = 'tbit.to';
 
     for(let i = 0; i < document.links.length; i++) {
